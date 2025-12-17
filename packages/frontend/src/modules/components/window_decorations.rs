171e7f95003c01f26ece5cb38db7982763da3626
= <seppi@neureiter.bayern>
2025-12-17 21:32:38 +0100

    added extra window decorations

--- packages/frontend/src/modules/components/window_decorations.rs
+++ packages/frontend/src/modules/components/window_decorations.rs
@@ -3,14 +3,35 @@ use super::utils::*;
 #[cfg(not(target_family = "wasm"))]
 use dioxus::desktop::window;
 
-
 #[cfg(not(target_family = "wasm"))]
 #[item]
 pub fn WindowDecorations() -> Element {
+    let get_fullscreen_icon = || {
+        if window().is_maximized() {
+            return FullscreenIcon();
+        } else {
+            return FullscreenExitIcon();
+        }
+    };
+
+    let mut fullscreen_icon = use_signal(|| get_fullscreen_icon());
+
     rsx! {
         div {
             section {
-                article { class: "wrapper", onclick: |_| { window().close() }, CloseIcon {} }
+                if window().is_minimizable() {
+                    button { onclick: |_| { window().set_minimized(true) }, MinimizeIcon {} }
+                }
+                if window().is_maximizable() {
+                    button {
+                        onclick: move |_| {
+                            window().toggle_maximized();
+                            fullscreen_icon.set(get_fullscreen_icon());
+                        },
+                        {fullscreen_icon()}
+                    }
+                }
+                button { onclick: |_| { window().close() }, CloseIcon {} }
             }
         }
     }
