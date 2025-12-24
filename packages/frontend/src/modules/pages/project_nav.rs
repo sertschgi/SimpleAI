ebcc8410b04e9465c2bb0a544cffd38c420cb59f
= <seppi@neureiter.bayern>
2025-12-24 11:36:13 +0100

    added edit to projects_nav

--- packages/frontend/src/modules/pages/project_nav.rs
+++ packages/frontend/src/modules/pages/project_nav.rs
@@ -5,6 +5,7 @@ pub fn ProjectNav(id: uuid::Uuid) -> Element {
     rsx! {
         main {
             NavigationPage {
+                NavigationLink { to: Route::Edit { id }, label: "edit", EditIcon {} }
                 NavigationLink { to: Route::Editor { id }, label: "onnx editor", ProjectsIcon {} }
             }
         }
